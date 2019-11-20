631caaac480d6c8b663ecd2c4512e03de3768df4
makarenkoj <makarenkoj@gmail.com>
2019-11-21 00:07:35 +0200

    check errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,24 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # настройка для работы девайза при правке профиля юзера
+  # Настройка для работы девайза при правке профиля юзера
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # хелпер метод, доступный во вьюхах
+  # Хелпер метод, доступный во вьюхах
   helper_method :current_user_can_edit?
 
+  # Настройка для девайза — разрешаем обновлять профиль, но обрезаем
+  # параметры, связанные со сменой пароля.
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
         :account_update,
         keys: [:password, :password_confirmation, :current_password]
     )
+  end
+
+  # Вспомогательный метод, возвращает true, если текущий залогиненный юзер
+  # может править указанное событие
+  def current_user_can_edit?(event)
+    user_signed_in? && event.user == current_user
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
+# (с) goodprogrammer.ru
+#
+# Контроллер, управляющий пользователями
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  # Встроенный в девайз фильтр — посылает незалогиненного пользователя
+  before_action :authenticate_user!, except: [:show]
 
+  # Задаем объект @user для шаблонов и экшенов
+  before_action :set_current_user, except: [:show]
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -18,8 +25,8 @@ class UsersController < ApplicationController
 
   private
 
-  def set_user
-    @user = User.find(params[:id])
+  def set_current_user
+    @user = current_user
   end
 
   def user_params
