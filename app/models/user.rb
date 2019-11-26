55db04f2839983d004213ae6a528db737df30974
makarenkoj <makarenkoj@gmail.com>
2019-11-27 00:22:29 +0200

    create comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
 
   has_many :events
 
+  has_many :comments, dependent: :destroy
+
   validates :name, presence: true, length: {maximum: 35}
 
   before_validation :set_name, on: :create
