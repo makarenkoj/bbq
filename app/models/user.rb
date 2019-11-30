467896c868a82e8d849fdd770f786aef451148ec
makarenkoj <makarenkoj53@gmail.com>
2019-11-30 18:43:07 +0200

    create subscriptions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_many :events
-
   has_many :comments, dependent: :destroy
+  has_many :subscriptions
 
   validates :name, presence: true, length: {maximum: 35}
 
