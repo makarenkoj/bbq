f2329899323805dd5290714c261f5b11628c79bc
makarenkoj <makarenkoj53@gmail.com>
2019-12-03 15:29:23 +0200

    user not subscribe, email check

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -9,6 +9,10 @@ class Subscription < ApplicationRecord
   validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
   validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
 
+  validate :user_organizer, on: :create, if: -> { user.present? }
+  #валидация на залогиненый емаил
+  validate :user_email_uniq, on: :create, unless: -> { user.present? }
+
   def user_name
     if user.present?
       user.name
@@ -24,4 +28,12 @@ class Subscription < ApplicationRecord
       super
     end
   end
+end
+
+def user_organizer
+  errors.add(:user_id, I18n.t('errors.organize_subscribe')) if user == event.user
+end
+
+def user_email_uniq
+  errors.add(:user_email, I18n.t('errors.user_email')) if User.all.where(email: user_email).any?
 end
