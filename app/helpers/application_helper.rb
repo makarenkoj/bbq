753c791d821fea7107e1d334fb53b91f7901f638
makarenkoj <makarenkoj53@gmail.com>
2019-12-11 22:19:03 +0200

    aded function load foto event

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,35 @@ module ApplicationHelper
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
+
+  # Возвращает адрес рандомной фотки события, если есть хотя бы одна
+  # Или ссылку на картинку по умолчанию
+  def event_photo(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.url
+    else
+      asset_path('event.jpg')
+    end
+  end
+
+  # Возвращает миниатюрную версию фотки
+  def event_thumb(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.thumb.url
+    else
+      asset_path('event_thumb.jpg')
+    end
+  end
+
+  def user_avatar_thumb(user)
+    if user.avatar.file.present?
+      user.avatar.thumb.url
+    else
+      asset_path('user.png')
+    end
+  end
 end
