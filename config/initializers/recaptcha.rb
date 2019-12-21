334c2fcc6799c9c772b840245d52b3745219ab11
makarenkoj <makarenkoj53@gmail.com>
2019-12-21 23:51:14 +0200

    config recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key = '6LcdKsgUAAAAACS2r-l_okEvPBeV_6kKSfW0Ltdh'
-  config.secret_key = '6LcdKsgUAAAAABfwoI9RYpavOxiRS6Ifb0lPYdhq'
+  config.site_key = 'RECAPTCHA_BBQ_PUBLIC_KEY'
+  config.secret_key = 'RECAPTCHA_BBQ_PRIVATE_KEY'
 end
