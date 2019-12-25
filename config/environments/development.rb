df92346879168cc2caf677703bbe36531410e0f2
makarenkoj <makarenkoj@gmail.com>
2019-12-25 18:13:27 +0200

    email seating

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
       port: '587',
-      user_name: 'apps.info.4.you@gmail.com', #почта
-      password: '', #удалить пароль
+      user_name: ENV['EMAIL_FOR_APPS'], #почта
+      password: ENV['PASSWORD_FOR_EMAIL'], #удалить пароль
       authentication: 'plain',
       enable_starttls_auto: true
   }
